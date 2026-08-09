e6749f217acf003daaf58cd0ebb413d1ad6470f4
Kunal Rawat <rkrawat462@gmail.com>
2026-08-09 11:30:01 +0530

    Time: 0 ms (100%), Space: 43.8 MB (47.31%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int search(int[] a, int target) {
+        int n=a.length;
+        int low=0;
+        int high=n-1;
+        
+        while(low<=high){
+            int guess=(low+high)/2;
+            if(a[guess]==target){
+                return guess;
+            }
+            if(a[guess]>a[high]){
+                if(a[guess]<target){
+                    low=guess+1;
+                }
+                else{
+                    if(a[low]<=target){
+                        high=guess-1;
+
+                    }
+                    else{
+                        low=guess+1;
+                    }
+                }
+                continue;
+            }
+            else{
+                if(a[guess]>target){
+                    high=guess-1;
+                }
+                else{
+                    if(a[high]<target){
+                       high=guess-1;
+                    }
+                    else{
+                        low=guess+1;
+                    }
+                }
+            }
+
+        }
+        return -1;
+    }
+}
